473899b8d53d378e062f669aa47a879ca216ab87
TqLxQuanZ <quanpro777@hotmail.com>
2023-10-24 12:19:45 +0800

    v4.2.4 - Fixed horde sometimes doesn't spawn for players who login later than usual or reseted time. - Buffed excavator deposit amount.

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -28,6 +28,8 @@ onEvent('entity.death', (event) => {
 onEvent('player.logged_in', event => {
 	if (!event.player.isAlive()) return
 
+	HardCheckHordeEvent(event)
+
 	event.server.scheduleInTicks(20, callback => {
 		let firstAidData = {ForgeCaps:{"firstaid:cap_adv_dmg_mdl":event.player.persistentData.firstAidData}}
 		let loginHealth = event.player.persistentData.health
@@ -49,3 +51,29 @@ onEvent('block.break', event => {
         event.setXp(1);
     };
 });
+
+function HardCheckHordeEvent(event)
+{
+	let hordeEventData = event.player.getFullNBT().ForgeCaps["hordes:hordeevent"]
+	let serverDays = event.level.getLocalTime() / 24000.0
+
+	// Means the server already went ahead of the horde event day, and caused horde won't spawn for player anymore
+	// Setting it to the next server day to trigger it.
+	if (serverDays > hordeEventData.nextDay)
+	{
+		hordeEventData.nextDay = serverDays + 1;
+		event.player.mergeFullNBT({ForgeCaps:{"hordes:hordeevent":hordeEventData}});
+		return;
+	}
+
+	// There's a chance that the server time got set and the horde event are way too further away
+	// We take the server days to check if the amount differences are exceeding 20
+	// if so we hard update it to the next 20 server days to spawn.
+	if (serverDays - hordeEventData.nextDay < -20)
+	{
+		hordeEventData.day = serverDays
+		hordeEventData.nextDay = serverDays + 20;
+		event.player.mergeFullNBT({ForgeCaps:{"hordes:hordeevent":hordeEventData}});
+		return;
+	}
+}
