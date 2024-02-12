d308b8ddb1a8f5b8edea665128651afb7decec54
TqLxQuanZ <quanpro777@hotmail.com>
2024-02-12 20:42:08 +0800

    v5.0.0 - Fixed death doesn't retrigger the gasmask timer to reduce durability in wasteland.

--- kubejs/server_scripts/gasmask_check.js
+++ kubejs/server_scripts/gasmask_check.js
@@ -2,21 +2,23 @@
 console.info('Loaded gasmask_check.js')
 
 onEvent('player.logged_in', event => {
-	event.server.scheduleInTicks(200, event, (callback) => {
-		if (event.entity.block.biomeId == "biomesoplenty:wasteland" || 
-			event.entity.block.biomeId == "biomesoplenty:wooded_wasteland")
-		{
-			if (event.player.headArmorItem.id == "apocalypsenow:gasmask_helmet" ||
-				event.player.headArmorItem.id == "apocalypsenow:pkm_4gasmask_helmet" ||
-				event.player.headArmorItem.id == "apocalypsenow:pbf_hamstergasmask_helmet" ||
-				event.player.headArmorItem.id == "apocalypsenow:soldier_helmet")
-			{
-				event.player.damageEquipment(EquipmentSlot.HEAD, 1);
-			}
-		}
-		callback.reschedule();
-	})
+	TickEquipment(event);
 })
+
+onEvent('entity.death', event => {
+	if (event.entity.isPlayer())
+	{
+		event.entity.player.data.resetFlag = true;
+	}
+})
+
+onEvent('player.tick', event => {
+	if (event.player.data.resetFlag == true && event.player.isAlive())
+	{
+		event.player.data.resetFlag = false;
+		TickEquipment(event);
+	}
+});
 
 onEvent('item.right_click', event => {
 	if (event.item.id == "apocalypsenow:breathingfiltermodule")
@@ -31,3 +33,21 @@ onEvent('item.right_click', event => {
 		}
 	}
 })
+
+function TickEquipment(event)
+{
+	event.server.scheduleInTicks(200, event, (callback) => {
+		if (event.entity.block.biomeId == "biomesoplenty:wasteland" || 
+			event.entity.block.biomeId == "biomesoplenty:wooded_wasteland")
+		{
+			if (event.player.headArmorItem.id == "apocalypsenow:gasmask_helmet" ||
+				event.player.headArmorItem.id == "apocalypsenow:pkm_4gasmask_helmet" ||
+				event.player.headArmorItem.id == "apocalypsenow:pbf_hamstergasmask_helmet" ||
+				event.player.headArmorItem.id == "apocalypsenow:soldier_helmet")
+			{
+				event.player.damageEquipment(EquipmentSlot.HEAD, 1);
+			}
+		}
+		callback.reschedule();
+	})
+}
