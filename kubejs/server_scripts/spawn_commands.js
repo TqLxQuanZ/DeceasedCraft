f5391eb7f0e44bad5cdeefd80f8d561e0ae21b32
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-03-05 22:41:05 +0800

    v5.0.0 - Updated more structures.

--- kubejs/server_scripts/spawn_commands.js
+++ kubejs/server_scripts/spawn_commands.js
@@ -21,4 +21,15 @@ onEvent("command.registry", event => { //command registry event
 			return 1
 		})
 	)
+	
+	event.register(
+		Commands.literal("spawnHelicopter")
+		.requires(src => src.hasPermission(2))
+		.executes(ctx => {
+				const sourcePosition = ctx.source.position
+				Utils.server.runCommandSilent(`summon simpleplanes:helicopter ${sourcePosition.x()} ${sourcePosition.y()} ${sourcePosition.z()} {material:'biomesoplenty:stripped_dead_log',upgrades:{'simpleplanes:furnace_engine':{item:{Size:1,Items:[{Slot:0,id:'minecraft:coal_block',Count:18b}]}},'simpleplanes:shooter':{item:{Size:1,Items:[{Slot:0,id:'cgm:missile',Count:7b}]}},'simpleplanes:armor':{protection:0b},'simpleplanes:seats':{}}}`)
+				Utils.server.runCommandSilent(`setblock ${sourcePosition.x() - 0.5} ${sourcePosition.y() - 0.5} ${sourcePosition.z() - 0.5} minecraft:air`)
+				return 1
+			})
+		)
 })
