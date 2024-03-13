de7a71566a9b14329003a08c8c6070c544afc9c9
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-03-14 01:52:06 +0800

    v5.1.4 - Updated block break.js

--- kubejs/server_scripts/block_break.js
+++ kubejs/server_scripts/block_break.js
@@ -20,6 +20,12 @@ onEvent('block.right_click', (event) => {
     if (block == "minecraft:command_block" && !event.player.isCreativeMode())
     {
         block.set('minecraft:air');
+        if (event.block.biomeId == "biomesoplenty:wasteland" || 
+            event.block.biomeId == "biomesoplenty:wooded_wasteland")
+        {
+            event.server.runCommand(`summon simpleplanes:helicopter ${block.pos.x} ${block.pos.y} ${block.pos.z} {material:'biomesoplenty:stripped_dead_log',upgrades:{'simpleplanes:furnace_engine':{item:{Size:1,Items:[{Slot:0,id:'minecraft:coal_block',Count:18b}]}},'simpleplanes:shooter':{item:{Size:1,Items:[{Slot:0,id:'cgm:missile',Count:7b}]}},'simpleplanes:armor':{protection:0b},'simpleplanes:seats':{}}}`);
+            return;
+        }
         event.server.runCommand(`summon car:car ${block.pos.x} ${block.pos.y} ${block.pos.z}`);
 		return
     }
