db0a711863169e8fbed5bf9bffe5e74aff563878
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-03-14 12:11:25 +0800

    v5.1.4 - Fixed the zombie player can be targeted by the mutated enemies and potentially lost all items. (Thanks to Liopyu in KubeJS Server)

---
+++ kubejs/startup_scripts/zombie_player_ignore.js
@@ -0,0 +1,22 @@
+// priority: 0
+
+console.info('Loaded mutation_sickness_check.js')
+onForgeEvent('net.minecraftforge.event.entity.living.LivingChangeTargetEvent', event => {
+    //set the global changetarget function into here and restart your game once
+    global.changetarget(event)
+})
+
+/**
+ * 
+ * @param {Internal.LivingChangeTargetEvent} event 
+ */
+global.changetarget = event => {
+    //since this is a global event you can run /kubejs reload startup_scripts to reload whatever is in here without
+    //restarting the entire game!
+    let { entity, entity: { persistentData }, originalTarget, newTarget } = event
+    if (!entity.type.toString().includes("entity.mutationcraft")) return
+    if (newTarget == null) return
+    if (newTarget.type.toString().includes("entity.hordes")) {
+        event.setNewTarget(null)
+    }
+}
