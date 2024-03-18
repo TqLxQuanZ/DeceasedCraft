6e4d551714bd47ba1d366f66e78c743363e82c38
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-03-18 23:09:07 +0800

    v5.2.0 - Fixed sorting cause missing item if there are planks in the inventory.

--- kubejs/server_scripts/inventory_check.js
+++ kubejs/server_scripts/inventory_check.js
@@ -3,7 +3,10 @@ console.info('Loaded inventory_check.js')
 
 // Some players might cheat command block in their inventory, but we'll remove it anyhow if the player is not in creative mode
 onEvent('player.inventory.changed', (event) => {
+	// Workbench can cause crash coz no recipe exists.
 	event.player.inventory.clear("cgm:workbench")
+	// Plank can cause sorting issue
+	event.player.inventory.clear("apocalypsenow:plank")
 	if (!event.player.isCreativeMode())
 	{
 		event.player.inventory.clear("minecraft:command_block")
