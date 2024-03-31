adc603cae1c8c35e9e57ce5dbbbf02aad29461a4
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-03-31 11:28:31 +0800

    v5.3.2 - Fixed ocean biome command block doesn't spawn motorboat when right click.

--- kubejs/server_scripts/block_break.js
+++ kubejs/server_scripts/block_break.js
@@ -26,6 +26,18 @@ onEvent('block.right_click', (event) => {
             event.server.runCommand(`summon simpleplanes:helicopter ${block.pos.x} ${block.pos.y} ${block.pos.z} {material:'biomesoplenty:stripped_dead_log',upgrades:{'simpleplanes:furnace_engine':{item:{Size:1,Items:[{Slot:0,id:'minecraft:coal_block',Count:18b}]}},'simpleplanes:shooter':{item:{Size:1,Items:[{Slot:0,id:'cgm:missile',Count:7b}]}},'simpleplanes:armor':{protection:0b},'simpleplanes:seats':{}}}`);
             return;
         }
+        if (event.block.biomeId == "minecraft:deep_cold_ocean" || 
+            event.block.biomeId == "minecraft:deep_frozen_ocean" || 
+            event.block.biomeId == "minecraft:deep_lukewarm_ocean" || 
+            event.block.biomeId == "minecraft:deep_ocean" ||
+            event.block.biomeId == "minecraft:cold_ocean" || 
+            event.block.biomeId == "minecraft:frozen_ocean" || 
+            event.block.biomeId == "minecraft:lukewarm_ocean" || 
+            event.block.biomeId == "minecraft:ocean")
+        {
+            event.server.runCommand(`summon immersivepetroleum:speedboat ${block.pos.x} ${block.pos.y} ${block.pos.z} {tank:{amount:4000,fluid:'immersivepetroleum:gasoline'}}`);
+            return;
+        }
         event.server.runCommand(`summon car:car ${block.pos.x} ${block.pos.y} ${block.pos.z}`);
 		return
     }
