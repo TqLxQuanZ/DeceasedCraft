47c293312add9289e9a032265a6611aaa5f893c2
TqLxQuanZ <yeezhanquan1998@gmail.com>
2024-05-02 01:25:29 +0800

    v5.4.13 - Fixed several minibosses can spawn and stuck inside wall. - Otter no longer drowns in water.

--- kubejs/server_scripts/spawn_check.js
+++ kubejs/server_scripts/spawn_check.js
@@ -2,21 +2,13 @@
 console.info('Loaded spawn_check.js')
 
 onEvent('entity.hurt', event => {
-	if (event.getEntity().getType() == "mutationcraft:parasitic_shooter")
-	{
-		if (event.getSource().getType() == "inWall")
-		{
-			event.getEntity().remove();
-		}
-	}
-	if (event.getEntity().getType() == "mutationcraft:the_intoxicator")
-	{
-		if (event.getSource().getType() == "inWall")
-		{
-			event.getEntity().remove();
-		}
-	}
-	if (event.getEntity().getType() == "zombie_extreme:demolisher")
+	if (event.getEntity().getType() == "mutationcraft:parasitic_shooter" ||
+		event.getEntity().getType() == "mutationcraft:the_intoxicator" ||
+		event.getEntity().getType() == "zombie_extreme:demolisher" ||
+		event.getEntity().getType() == "zombie_extreme:rat_king" ||
+		event.getEntity().getType() == "zombie_extreme:patient_zero" ||
+		event.getEntity().getType() == "mutationcraft:human_herder" ||
+		event.getEntity().getType() == "mutationcraft:resenter")
 	{
 		if (event.getSource().getType() == "inWall")
 		{
@@ -25,9 +17,21 @@ onEvent('entity.hurt', event => {
 	}
 	if (event.getEntity().getType() == "alexsmobs:centipede_head" ||
 		event.getEntity().getType() == "alexsmobs:centipede_body" ||
-		event.getEntity().getType() == "alexsmobs:centipede_tail")
+		event.getEntity().getType() == "alexsmobs:centipede_tail" ||
+		event.getEntity().getType() == "zombie_extreme:demolisher" ||
+		event.getEntity().getType() == "zombie_extreme:rat_king" ||
+		event.getEntity().getType() == "zombie_extreme:patient_zero" ||
+		event.getEntity().getType() == "mutationcraft:human_herder" ||
+		event.getEntity().getType() == "mutationcraft:resenter")
 	{
 		if (event.getSource().getType() == "inWall" || event.getSource().getType() == "fall")
+		{
+			event.cancel();
+		}
+	}
+	if (event.getEntity().getType() == "crittersandcompanions:otter")
+	{
+		if (event.getSource().getType() == "drown")
 		{
 			event.cancel();
 		}
